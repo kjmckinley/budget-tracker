45c06820d4a7886405639d3affd59fda5046b5ed
Kyle <kj.mckinley@gmail.com>
2021-07-29 21:23:00 -0500

    added comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,17 @@
+
+// array of files to link and cache
 const FILE_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
     "./js/index.js"
 ]
 
+// Budget tracker app prefixes/variables
 const APP_PREFIX = "BudgetTracker-"
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
+// log the cache that is installing
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -17,6 +21,7 @@ self.addEventListener('install', function (e) {
     )
 });
 
+// activate and delete cache
 self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
@@ -38,6 +43,7 @@ self.addEventListener('activate', function (e) {
     )
 });
 
+// retrieve needed cache 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
@@ -55,5 +61,5 @@ self.addEventListener('fetch', function (e) {
 })
 
 
-
+// confirm that the service worker was registered in the console
 console.log('Service worker registered!')
